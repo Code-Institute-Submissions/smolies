c3594ccab13c237324bfb0b4c44e877ec9da1c16
katzur <zurawska.kasia@gmail.com>
2022-03-29 12:04:51 +0000

    Create general pet construction function

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Smolies - 90s style pet game
+
+from random import randrange
+
+class Pet(object):
+    """
+    Create a pet
+    """
+    fun_reduce = 3
+    fun_max = 10
+    fun_warning = 3
+    food_reduce = 2
+    food_max = 10
+    food_warning = 3
+    vocab = ['"Grr..."']
+
+    def __init__(self, name, pet_type):
+        self.name = name
+        self.pet_type = pet_type
+        self.food = randrange(self.food_max)
+        self.fun = randrange(self.fun_max)
+        self.vocab = self.vocab[:]
