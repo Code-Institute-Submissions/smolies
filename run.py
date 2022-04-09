5d4d8ce5a111516d7b080322a259547806489bfc
katzur <zurawska.kasia@gmail.com>
2022-04-09 12:46:27 +0000

    Change game banner styling based on poor layout look on Heroku

--- run.py
+++ run.py
@@ -42,8 +42,7 @@ def create_pet():
     Prompt for different options of pet type
     """
     print(colored(pyfiglet.figlet_format(
-        "Smolies - pet game", width=200,), 'green', attrs=['bold']
-        ))
+        "Smolies - pet game", width=200,), 'green'))
     # get the input of what type of pet is this
     pet_type = ""
 
